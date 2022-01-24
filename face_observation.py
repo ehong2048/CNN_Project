fad3e27f42b3c7dec76577891dc5d70d6e30a385
ehong2048 <ehong2048@gmail.com>
2022-01-23 21:41:15 -0800

    Added code to read and process the image labels

--- face_observation.py
+++ face_observation.py
@@ -6,20 +6,27 @@
 from tensorflow.keras import utils
 
 def load_labels(labels_filename = str):
+    """
     # reads text from file based on passed in labels filename and creates a list of lists of each image's labels
     # each label is in the form of a one-hot encoded vector with 40 attributes
+   """
     label_file = open(labels_filename, 'r')
     labels = list(label_file.readlines()) # list of the labels of each image
+    processed_labels = []
     for i in range(2, len(labels)):
-        img_labels = labels[i] # labels of indexed image/row
-        print(img_labels)
+        img_labels = labels[i] # labels of the indexed image/row
+        img_labels = img_labels.split() #turns labels string into list
+        processed_labels.append(img_labels)
+    
+    print(len(processed_labels))
+    return processed_labels
 
-    # next get rid of first two rows, and then 1st item in list
-    return
+labels = load_labels('anno/list_attr_celeba.txt')
 
 """
 train = utils.image_dataset_from_directory(
     'img_align_celeba',
+    labels = labels,
     label_mode = 'categorical',
     image_size=(218, 178),
     shuffle = True,
@@ -30,6 +37,7 @@ train = utils.image_dataset_from_directory(
 
 val = utils.image_dataset_from_directory(
     'img_align_celeba',
+    labels = labels,
     label_mode = 'categorical',
     image_size=(250, 250),
     shuffle = True,
@@ -39,4 +47,4 @@ val = utils.image_dataset_from_directory(
 )
 """
 
-load_labels('anno/list_attr_celeba.txt')
+
