79b7b1f5bbee3db1e4187ba82da1d14414b6c95e
ehong2048 <ehong2048@gmail.com>
2022-01-23 21:38:05 -0800

    More images

--- face_observation.py
+++ face_observation.py
@@ -9,13 +9,15 @@ def load_labels(labels_filename = str):
     # reads text from file based on passed in labels filename and creates a list of lists of each image's labels
     # each label is in the form of a one-hot encoded vector with 40 attributes
     label_file = open(labels_filename, 'r')
-    labels = label_file.readlines()
-    for i in range[2:]:
-        img = labels[i]
-        
+    labels = list(label_file.readlines()) # list of the labels of each image
+    for i in range(2, len(labels)):
+        img_labels = labels[i] # labels of indexed image/row
+        print(img_labels)
+
     # next get rid of first two rows, and then 1st item in list
     return
 
+"""
 train = utils.image_dataset_from_directory(
     'img_align_celeba',
     label_mode = 'categorical',
@@ -35,3 +37,6 @@ val = utils.image_dataset_from_directory(
     validation_split = 0.3,
     subset = 'validation',
 )
+"""
+
+load_labels('anno/list_attr_celeba.txt')
