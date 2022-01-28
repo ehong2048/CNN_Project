a2af08a62105df0e546b39cf05575514b9b7a75f
ehong2048 <ehong2048@gmail.com>
2022-01-28 10:56:49 -0800

    Worked on processing data to only show one attribute for each image

--- face_observation.py
+++ face_observation.py
@@ -1,9 +1,10 @@
-from xml.dom import ValidationErr
 import tensorflow.keras.models as models
 import tensorflow.keras.layers as layers
 import tensorflow.keras.losses as losses
 import tensorflow.keras.optimizers as optimizers
 from tensorflow.keras import utils
+
+# FOR NEXT TIME: Maybe just change to one attribute so that the dataset doesn't overlap (i.e. don't do multilabel classification)
 
 def load_labels(labels_filename = str):
     """
@@ -12,48 +13,60 @@ def load_labels(labels_filename = str):
    """
     label_file = open(labels_filename, 'r')
     labels = list(label_file.readlines()) # list of the labels for each image
-    processed_labels = [] # initiates list of processed labels for each image
-    for i in range(2, len(labels)):
-        img_labels = labels[i] # labels of the indexed image/row
-        img_labels = img_labels.split() #turns labels string into list
-        processed_labels.append(img_labels[1:])
+    processed_labels = [] # initiates list of processed labels for the smiling trait of each image
+    #for i in range(2, len(labels)):
+    for i in range(2, 5002):
+        img_labels = labels[i, 32] # labels of the indexed image/row
+        # img_labels = img_labels.split() #turns labels string into list
+        processed_labels.append(img_labels)
     
-    print(len(processed_labels))
+    print(f'Number of Images: {len(processed_labels)}')
+    print(type(processed_labels))
     return processed_labels
 
 labels = load_labels('anno/list_attr_celeba.txt')
 
+print("--Retreive training data--")
 train = utils.image_dataset_from_directory(
-    'img_align_celeba',
-    labels = labels,
-    label_mode = 'categorical',
+    '/Users/emma/Documents/CNN_Project/img_align_celeba',
+    labels = None,
+    #label_mode = 'categorical',
     image_size=(218, 178),
     shuffle = True,
     seed = 0,
     validation_split = 0.3,
     subset = 'training',
 )
+print(len(train))
 
+print("--Retreive val data--")
 val = utils.image_dataset_from_directory(
     'img_align_celeba',
-    labels = labels,
-    label_mode = 'categorical',
-    image_size=(250, 250),
+    labels = None,
+    # label_mode = 'categorical',
+    image_size=(218, 178),
     shuffle = True,
     seed = 0,
     validation_split = 0.3,
     subset = 'validation',
 )
 
+
 class Net():
     def __init__(self, image_size):
         self.model = models.Sequential() # layers are in sequence
+        self.model.add(layers.Conv2D(8, 13, strides = 3, input_shape = image_size, activation = 'relu')) # (output depth, frame size, kwargs)
+        self.model.add(layers.MaxPool2D(pool_size = 2)) # (frame size, kwargs, strides equals frame size as default
+        self.model.add(layers.Conv2D(16, 3))
+        self.model.add(layers.MaxPool2D(pool_size = 2))
         # insert other layers
         self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(1024, activation = 'relu'))
+        self.model.add(layers.Dense(256, activation = 'relu'))
         self.model.add(layers.Dense(40, activation = 'softmax'))
         
         self.loss = losses.MeanSquaredError()
-        self.optimizers(optimizers.Adam(learning_rate = 0.001))
+        self.optimizer = optimizers.Adam(learning_rate = 0.001)
 
         self.model.compile(
             loss = self.loss,
@@ -63,11 +76,14 @@ class Net():
 
     def __str__(self):
         self.model.summary()
-        return
+        return ""
 
 net = Net((218, 178, 3))
 print(net)
 
+net.model.fit(train, labels[0:3500])
+
+"""
 net.fit(
     train,
     batch_size = 32,
@@ -76,3 +92,4 @@ net.fit(
     validation_data = val,
     validation_batch_size = 32
 )
+"""
